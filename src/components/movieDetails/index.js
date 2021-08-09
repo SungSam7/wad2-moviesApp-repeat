94fa7f5376e7867210afe5e7323b2e86f9d355aa
Daniel McCarthy <danielmccarthy20@gmail.com>
2021-08-09 16:29:00 +0100

    Added routing and live data from API

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -25,7 +25,7 @@ export default ({ movie }) => {
         <li key="gh" className="list-group-item list-group-item-dark">
           Genres
         </li>
-        {movie.genres.map(g => (
+        {movie.genres.map((g) => (
           <li key={g.name} className="list-group-item">
             {g.name}
           </li>
@@ -35,7 +35,7 @@ export default ({ movie }) => {
         <li key="slh" className="list-group-item list-group-item-dark">
           Spoken Languages
         </li>
-        {movie.spoken_languages.map(lang => (
+        {movie.spoken_languages.map((lang) => (
           <li key={lang.name} className="list-group-item">
             {lang.name}
           </li>
@@ -45,11 +45,16 @@ export default ({ movie }) => {
         <li key="pch" className="list-group-item list-group-item-dark">
           Production Companies
         </li>
-        {movie.production_companies.map(pc => (
+        {movie.production_companies.map((pc) => (
           <li key={pc.name} className="list-group-item">
             {pc.name}
           </li>
         ))}
+      </ul>
+      <ul className="list-group list-group-horizontal">
+        <li key="pcc" className="list-group-item list-group-item-dark">
+          Production Countries
+        </li>
       </ul>
     </>
   );
